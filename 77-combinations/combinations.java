5f9fc7f30572566c76b2fa432a72457cb9e50467
Rishabh tripathi <rishabhtripathi763@gmail.com>
2026-07-06 10:59:32 +0530

    Time: 21 ms (25.49%) | Memory: 98.2 MB (13.40%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,25 +1,31 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-  List<List<Integer>>list=new ArrayList<>();
+
+        List<List<Integer>> list = new ArrayList<>();
+        SolveIt(new ArrayList<>(), list, n, k, 1);
+        return list;
+    }
+
+    public static void SolveIt(List<Integer> curr,
+                               List<List<Integer>> list,
+                               int n,
+                               int k,
+                               int idx) {
+
+        if (curr.size() == k) {
+            list.add(new ArrayList<>(curr));
+            return;
+        }
+
+        if (idx > n)
+            return;
+
+        
+        curr.add(idx);
+        SolveIt(curr, list, n, k, idx + 1);
 
       
-         SolveIt(new ArrayList<>(),list,n,k,1);
-         return list;
-    }
-public static void SolveIt(List<Integer>curr,List<List<Integer>>list,int n,int k,int idx){
-
-
-if(curr.size()==k){
-list.add(new ArrayList<>(curr));
-return;
-     }
-
-for(int i=idx;i<=n;i++){
-    curr.add(i);
-    SolveIt(curr,list,n,k,i+1);
-    curr.remove(curr.size()-1);
-}
-
-
+        curr.remove(curr.size() - 1);
+        SolveIt(curr, list, n, k, idx + 1);
     }
 }
