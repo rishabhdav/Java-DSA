7c99810aa55f47e93f97beb90d90880d167760f3
Rishabh tripathi <rishabhtripathi763@gmail.com>
2026-07-08 22:19:34 +0530

    Time: 3 ms (10.55%) | Memory: 72.5 MB (48.41%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean lemonadeChange(int[] bills) {
         int five=0;
         int ten=0;
-        int tw=0;
+       
         for(int i=0;i<bills.length;i++){
             if(bills[i]==5) five++;
            else  if(bills[i]==10) ten++;
-            else tw++;
+           
 
             int change=bills[i]-5;
             if(change==5){
