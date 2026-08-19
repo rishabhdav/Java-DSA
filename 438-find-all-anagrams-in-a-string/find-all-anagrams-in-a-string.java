eff2940f259c9f4cc3fdc826078d2b57d018e3db
Rishabh tripathi <rishabhtripathi763@gmail.com>
2026-08-19 20:50:19 +0530

    Time: 9 ms (92.51%) | Memory: 46.6 MB (92.26%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,25 +1,42 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-   
-int[] arr1=new int[26];
-int [] arr2=new int[26];
 
-List<Integer>list=new ArrayList<>();
-   if(s.length()<p.length())   return list;
-for(int i=0;i<p.length();i++){
-    arr1[p.charAt(i)-'a']++;
-    arr2[s.charAt(i)-'a']++;
-}
-if(Arrays.equals(arr1,arr2))list.add(0);
-int k=0;
-for(int j=p.length();j<s.length();j++){
-    arr2[s.charAt(k)-'a']--;
-    k++;
-    arr2[s.charAt(j)-'a']++;
-    if(Arrays.equals(arr1,arr2))list.add(k);
-}
-return list;
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
 
-}
+        List<Integer> list = new ArrayList<>();
+        if (s.length() < p.length())
+            return list;
+
+        int k = p.length();
+
+        for (int i = 0; i < k; i++) {
+            arr1[p.charAt(i)-'a']++;
+        }
+
+        int i = 0;
+        int j = 0;
+        int n = s.length();
+        while (j < n) {
+
+            arr2[s.charAt(j)-'a']++;
+
+            if (j - i + 1 > k) {
+                arr2[s.charAt(i)-'a']--;
+                i++;
+            }
+
+            if (j - i + 1 == k) {
+                if(Arrays.equals(arr1,arr2)) {
+                    list.add(i);
+                }
+
+            }
+
+            j++;
+        }
+        return list;
 
     }
+
+}
