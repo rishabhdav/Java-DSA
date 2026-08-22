88a65009f14493ae5e04169c80e6d85726e3198b
Rishabh tripathi <rishabhtripathi763@gmail.com>
2026-08-22 08:53:45 +0530

    Time: 115 ms (27.98%) | Memory: 54.9 MB (10.16%) - LeetSync

--- 753-open-the-lock/open-the-lock.java
+++ 753-open-the-lock/open-the-lock.java
@@ -1,47 +1,50 @@
-class Pair{
+class Pair {
     String st;
     int level;
-    Pair(String st,int level){
-        this.st=st;
-        this.level=level;
+
+    Pair(String st, int level) {
+        this.st = st;
+        this.level = level;
     }
 }
 
 class Solution {
     public int openLock(String[] deadends, String target) {
-        Queue<Pair> q=new LinkedList<>();
-        Set<String>visited=new HashSet<>();
-        for(int i=0;i<deadends.length;i++){
+        Queue<Pair> q = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        for (int i = 0; i < deadends.length; i++) {
             visited.add(deadends[i]);
         }
-        if(visited.contains("0000")||visited.contains(target)) return -1;
-        q.add(new Pair("0000",0));
+        if (visited.contains("0000") || visited.contains(target))
+            return -1;
+        q.add(new Pair("0000", 0));
 
-        while(!q.isEmpty()){
-             Pair pa=q.poll();
+        while (!q.isEmpty()) {
+            Pair pa = q.poll();
 
-             if(pa.st.equals(target)) return pa.level;
+            if (pa.st.equals(target))
+                return pa.level;
 
-             for(int i=0;i<4;i++){
-              int val=pa.st.charAt(i)-'0';
+            for (int i = 0; i < 4; i++) {
+                int val = pa.st.charAt(i) - '0';
 
-                StringBuilder posstr=new StringBuilder(pa.st);
-               int postval=(val+1)%10;
-              int  negval=(val+9)%10;
-                posstr.setCharAt(i,(char)(postval+'0'));
-                StringBuilder negstr=new StringBuilder(pa.st);
-                negstr.setCharAt(i,(char)(negval+'0'));
-                if(!visited.contains(posstr.toString())){
+                StringBuilder posstr = new StringBuilder(pa.st);
+                int postval = (val + 1) % 10;
+                int negval = (val + 9) % 10;
+                posstr.setCharAt(i, (char) (postval + '0'));
+                StringBuilder negstr = new StringBuilder(pa.st);
+                negstr.setCharAt(i, (char) (negval + '0'));
+                if (!visited.contains(posstr.toString())) {
                     visited.add(posstr.toString());
-                    q.add(new Pair(posstr.toString(),pa.level+1));
+                    q.add(new Pair(posstr.toString(), pa.level + 1));
                 }
-                 if(!visited.contains(negstr.toString())){
+                if (!visited.contains(negstr.toString())) {
                     visited.add(negstr.toString());
-                    q.add(new Pair(negstr.toString(),pa.level+1));
+                    q.add(new Pair(negstr.toString(), pa.level + 1));
                 }
-                
-             }
+
+            }
         }
-return -1;
+        return -1;
     }
 }
